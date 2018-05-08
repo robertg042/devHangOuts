e249731cc1c2497de84f5b74f72ac271dab5e9ba
Robert Górnicki <github@gorki.net.pl>
2018-05-08 02:26:25 +0200

    f-profile_api: routes deleting experience and education added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -228,16 +228,15 @@ router.post(
   "/experience",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
+    const { errors, isValid } = validateExperienceInput(req.body);
+
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
     Profile.findOne({ user: req.user.id })
       .then(profile => {
         if (profile) {
-          console.log(req.body);
-          const { errors, isValid } = validateExperienceInput(req.body);
-
-          if (!isValid) {
-            return res.status(400).json(errors);
-          }
-
           const newExp = {
             title: req.body.title,
             company: req.body.company,
@@ -278,16 +277,15 @@ router.post(
   "/education",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
+    const { errors, isValid } = validateEducationInput(req.body);
+
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
     Profile.findOne({ user: req.user.id })
       .then(profile => {
         if (profile) {
-          console.log(req.body);
-          const { errors, isValid } = validateEducationInput(req.body);
-
-          if (!isValid) {
-            return res.status(400).json(errors);
-          }
-
           const newEdu = {
             school: req.body.school,
             degree: req.body.degree,
@@ -300,6 +298,80 @@ router.post(
 
           // Add to experience array
           profile.education.unshift(newEdu);
+
+          profile
+            .save()
+            .then(profile => res.json(profile))
+            .catch(err => {
+              console.log(err);
+
+              return res.status(500).json({ error: ERROR_INTERNAL_ERROR });
+            });
+        } else {
+          return res.status(404).json({ error: ERROR_PROFILE_NOT_FOUND });
+        }
+      })
+      .catch(err => {
+        console.log(err);
+
+        return res.status(404).json({ error: ERROR_PROFILE_NOT_FOUND });
+      });
+  }
+);
+
+// @route DELETE api/profile/experience/:exp_id
+// @desc Delete experience from profile
+// @access Private
+router.delete(
+  "/experience/:exp_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (profile) {
+          // Get experience index
+          const expIndex = profile.experience
+            .map(exp => exp.id)
+            .indexOf(req.params.exp_id);
+
+          profile.experience.splice(expIndex, 1);
+
+          profile
+            .save()
+            .then(profile => res.json(profile))
+            .catch(err => {
+              console.log(err);
+
+              return res.status(500).json({ error: ERROR_INTERNAL_ERROR });
+            });
+        } else {
+          return res.status(404).json({ error: ERROR_PROFILE_NOT_FOUND });
+        }
+      })
+      .catch(err => {
+        console.log(err);
+
+        return res.status(404).json({ error: ERROR_PROFILE_NOT_FOUND });
+      });
+  }
+);
+
+// @route DELETE api/profile/education/:edu_id
+// @desc Delete education from profile
+// @access Private
+router.delete(
+  "/education/:edu_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (profile) {
+          // Get education index
+          const eduIndex = profile.education
+            .map(edu => edu.id)
+            .indexOf(req.params.edu_id);
+
+          profile.education.splice(eduIndex, 1);
 
           profile
             .save()
