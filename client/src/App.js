ba5e2842e0575df6ab98188ca33de05bdf3a7156
Robert Górnicki <github@gorki.net.pl>
2018-05-11 17:43:48 +0200

    client: lato and opensans fonts added

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,8 @@
 import React, { Component } from "react";
-import "./App.css";
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1>React</h1>
-      </div>
-    );
+    return <div className="App">App</div>;
   }
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import "./assets/fonts/font-awesome/fontawesome-all.global.css";
+import "./assets/fonts/open-sans/opensans.global.css";
+import "./assets/fonts/lato/latofonts.global.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
