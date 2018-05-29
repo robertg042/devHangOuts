9aee875e2349147f089848a992a2ba6915eb73af
Robert Górnicki <github@gorki.net.pl>
2018-05-29 06:32:01 +0200

    client: about section added

--- client/src/components/Profile/ProfileAbout/ProfileAbout.js
+++ client/src/components/Profile/ProfileAbout/ProfileAbout.js
@@ -1,16 +1,30 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
+import classes from "./ProfileAbout.css";
+import { isEmpty, capitalizeFirstLetter } from "../../../shared/utils";
+
 class ProfileAbout extends Component {
   render() {
+    const { profile } = this.props;
+    const { user, bio } = profile;
+
+    // eslint-disable-next-line
+    const firstName = user.name.trim().split(" ")[0];
+
     return (
-      <div>
-        ProfileAbout
+      <div className={classes.ProfileAbout}>
+        <div className={classes.Header}>
+          {capitalizeFirstLetter(firstName)}&apos;s bio
+        </div>
+        {!isEmpty(bio) ? <div className={classes.Bio}>{bio}</div> : <div className={classes.Bio}>{firstName} has no bio</div>}
       </div>
     );
   }
 }
 
-ProfileAbout.propTypes = {};
+ProfileAbout.propTypes = {
+  profile: PropTypes.object.isRequired
+};
 
 export default ProfileAbout;
